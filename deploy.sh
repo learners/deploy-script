3e4ebe3fee1f70b5dfc0e62d81950945f287e3bf
linmingjie <linmingjie@pcitech.com>
2022-11-11 11:18:57 +0800

    feat: 优化部署脚本

--- deploy.sh
+++ deploy.sh
@@ -15,6 +15,9 @@ app_list=(
   viid-data-frontend
   epidemic-investigation-frontend
   portrait-portal-frontend
+  "multi-info-frontend 多维、潍坊应用"
+  "video-application-frontend 智能视频服务平台"
+  "video-application-frontend-xc 老联网、信创"
 )
 
 start () {
@@ -22,10 +25,14 @@ start () {
     ##
     ## 定制化打包命令
     ##
-    app_name="$1" # 应用名
+    app_name="$(echo $1 | sed -e 's/^\([^[:space:]]*\).*/\1/g')" # 应用名
     work_dir="$base_dir/$app_name" # 项目路径
+    build_script="yarn build:uni" # 构建命令
+    dest_dir="" # 构建目录，默认dist
     ssh_server="root@172.25.22.178" # 服务器地址
     target_dir="/opt/frontend/$app_name" # 部署到服务器的路径
+    backup_dir="" # 备份目录，相对于$target_dir的路径，默认dist--时间戳，例如dist--202211111035
+    after_script="sh $target_dir/bin/run.sh restart" # 部署命令
 
     if [ $app_name = "vehicles-frontend" ]; then
       build_script="yarn build:ga"
@@ -37,10 +44,35 @@ start () {
       $app_name = "portrait-portal-frontend" \
     ]; then
       build_script="yarn build:uni"
+
+    elif [ $app_name = "multi-info-frontend" ]; then
+      work_dir="$base_dir/temp/$app_name"
+      ssh_server="root@172.25.21.205"
+      target_dir="/opt/frontend/micro-multi-frontend/$app_name"
+    elif [ $app_name = "video-application-frontend" ]; then
+      work_dir="$base_dir/temp/$app_name"
+      ssh_server="root@172.25.23.56"
+      target_dir="/opt/video/$app_name"
+    elif [ $app_name = "video-application-frontend-xc" ]; then
+      work_dir="$base_dir/temp/video-application-frontend"
+      dest_dir="dist/*" # 将dist目录下的所有文件拷贝到$target_dir
+      ssh_server="root@172.25.20.25"
+      target_dir="/opt/uspp-web/apache-tomcat-7.0.109/webapps/video-application-frontend-xc"
+      backup_dir="../dist--$(date '+%Y%m%d%H%M')"
+      after_script=""
+
     fi
 
     # 开始部署应用
-    deploy_app $app_name $work_dir "$build_script" $ssh_server $target_dir
+    deploy_app \
+      $app_name \
+      $work_dir \
+      "$build_script" \
+      $ssh_server \
+      "$dest_dir" \
+      $target_dir \
+      "$backup_dir" \
+      "$after_script"
 
     # 多行注释
     : '
@@ -95,9 +127,12 @@ deploy_app () {
   ## 文件传输
   ##
   local ssh_server=$4
-  local source_dir="${PWD}/dist"
-  local target_dir=$5
-  local backup_dir="./dist--$(date '+%Y%m%d%H%M')"
+  local source_dir="${PWD}/${5:-dist}"
+  local target_dir=$6
+  local backup_dir=${7:-./dist--$(date '+%Y%m%d%H%M')}
+
+  local ssh_script="ssh $ssh_server \"$8\""
+  local after_script=$([ -n "$8" ] && echo "$ssh_script")
 
   echo "[$app_name] 开始传输文件"
   # 将dist目录下所有文件同步到服务器，并重启服务
@@ -107,8 +142,7 @@ deploy_app () {
     --exclude=*.sh \
     --backup \
     --backup-dir=$backup_dir $source_dir $ssh_server:$target_dir && \
-      echo "[$app_name] 重启服务" && \
-      ssh $ssh_server "sh $target_dir/bin/run.sh restart"
+      echo "[$app_name] 正在部署" && $after_script
 
   if [ $? -eq 0 ]; then
     echo "[$app_name] 部署成功"
@@ -167,3 +201,13 @@ do
   esac
   shift
 done
+
+# 三元表达式
+: '
+result=${1:-richard} # 如果$1不为空，则返回$1，否则返回richard
+result=$([ "$1" == 1 ] && echo $1 || echo "0") # 如果$1等于1，则返回1，否则返回0
+
+# [ -n 参数 ] 可以表示该参数是否被赋值
+# [ -n "$1" ] 等价于 [[ -n $1 ]]
+result=$(if [ -n "$1" ]; then echo $1; else echo "未赋值"; fi)
+'
